84014e52e8206eae389105a531678e59177b3af5
njm1250 <njm1250@gmail.com>
2023-10-28 09:12:03 +0900

    Added MySQL AWS RDS connection test codes

--- src/main/java/com/github/njm1250/blog/entity/TestEntity.java
+++ src/main/java/com/github/njm1250/blog/entity/TestEntity.java
@@ -3,12 +3,12 @@ package com.github.njm1250.blog.entity;
 import javax.persistence.*;
 
 @Entity // JPA가 관리하는 엔티티임을 나타냅니다.
-@Table(name="user")
+@Table(name="users")
 public class TestEntity {
 
     @Id // 이 필드가 테이블의 기본 키임을 나타냅니다.
     @GeneratedValue(strategy = GenerationType.AUTO) // 기본 키가 자동 생성됨을 나타냅니다.
-    private Long iduser; // 이 필드는 test 테이블의 iduser 컬럼에 매핑됩니다.
+    private Long user_id; // 이 필드는 test 테이블의 iduser 컬럼에 매핑됩니다.
 
     // 추가 필드는 해당 테이블의 다른 컬럼을 나타냅니다.
 
@@ -18,11 +18,11 @@ public class TestEntity {
 
     // getters and setters
     public Long getIduser() {
-        return iduser;
+        return user_id;
     }
 
     public void setIduser(Long iduser) {
-        this.iduser = iduser;
+        this.user_id = iduser;
     }
 
     // 추가 필드에 대한 getters 및 setters...
