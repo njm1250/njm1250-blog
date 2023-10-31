5633a44b7e5a0f786d647246865b98b9102124f0
박준혁 <njm1250@gmail.com>
2023-10-31 22:09:44 +0900

    feat: post entity and dto

---
+++ src/main/java/com/github/njm1250/blog/dto/PostDto.kt
@@ -0,0 +1,11 @@
+package com.github.njm1250.blog.dto
+
+import lombok.Builder
+import lombok.Getter
+import lombok.Setter
+
+@Getter
+@Setter
+@Builder
+class PostDto {
+}
---
+++ src/main/java/com/github/njm1250/blog/entity/Post.java
@@ -0,0 +1,14 @@
+package com.github.njm1250.blog.entity;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@NoArgsConstructor
+@Builder
+public class Post {
+}
