c8d18652feb23d0c98d2dc3b3f87635b00cf53a9
박준혁 <njm1250@gmail.com>
2023-11-16 21:57:21 +0900

    feat: setter getter

--- src/main/java/com/github/njm1250/blog/entity/Comment.java
+++ src/main/java/com/github/njm1250/blog/entity/Comment.java
@@ -1,7 +1,11 @@
 package com.github.njm1250.blog.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
+@Setter
+@Getter
 public class Comment {
 }
