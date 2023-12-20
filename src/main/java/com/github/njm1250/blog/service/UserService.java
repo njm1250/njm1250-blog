a9c849fe86dfcf2dec91007795e4b2e2e63a5dcd
njm1250 <njm1250@gmail.com>
2023-12-20 21:04:28 +0900

    refactor: 주석추가

--- src/main/java/com/github/njm1250/blog/service/UserService.java
+++ src/main/java/com/github/njm1250/blog/service/UserService.java
@@ -4,6 +4,19 @@ import com.github.njm1250.blog.dto.UserDto;
 import com.github.njm1250.blog.entity.User;
 
 public interface UserService {
+
+    /**
+     * 회원가입
+     *
+     * @param userDto UserDto
+     * @return void
+     */
     void signupUser(UserDto userDto);
+    /**
+     * 로그인
+     *
+     * @param userDto UserDto
+     * @return UserDto
+     */
     UserDto loginUser(UserDto userDto);
 }
